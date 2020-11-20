73c13dde07fc075f18b93a37ad9e518323cdc047
Hunter <kolasa.hunter@aol.com>
2020-11-20 14:40:07 -0500

    commit tree 54f301c918d0bca8fcd03fcf426347a3b0f4afb4

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+#individual badges
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+def batch_badge_creator(name_array)
+    final_array = []
+    name_array.each{|name| final_array << "Hello, my name is #{name}."}
+    final_array
+end
+def assign_rooms(name_array)
+    index = 1
+    secondary = []
+    name_array.each do |name|
+        secondary << "Hello, #{name}! You'll be assigned to room #{index}!"  
+        index += 1 
+    end
+    secondary
+end
+def printer(attendees)
+    batch_badge_creator(attendees).each do |greeting|
+        puts greeting
+    end
+    assign_rooms(attendees).each do |rooms|
+        puts rooms
+    end    
+end
